d333b5b561ae792ea8b485da0bc4042f124c9892
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-26 10:04:37 -0300

    changes page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,44 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Header from "@/components/Header";
+import Ads from "@/components/Ads";
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
-    <h1>Olá!</h1>
-  )
+    <div>
+      <Header title="Aluguel De Carros" />
+      <div>
+        <Ads
+          title="A Melhor Plataforma para Aluguel de Carros"
+          description="Facilidade em fazer um aluguel de carro com segurança e
+            confiabilidade. Claro a um preço baixo."
+          tag="Carro Alugado"
+        >
+          <Image
+            src="/assets/ads1.png"
+            alt="Imagen Ads carro alugado"
+            width={406}
+            height={116}
+          />
+        </Ads>
+        <Ads
+          title="Maneira fácil de alugar um carro a um preço baixo"
+          description="Fornecendo serviços de aluguel de carros baratos e instalações
+            seguras e confortáveis."
+          tag="Carro Alugado"
+        >
+          <Image
+            src="/assets/ads2.png"
+            alt="Imagem Ads carro alugado"
+            width={340}
+            height={108}
+          />
+        </Ads>
+      </div>
+      <h2>Catálogo de carros</h2>
+      <p>Explore os carros que você pode gostar</p>
+      <SearchBar />
+    </div>
+  );
 }

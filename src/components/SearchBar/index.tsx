99f9084f32fb05b6f6d958a0f1861bf5a7142d2e
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-28 11:20:30 -0300

    componente SearchBar change

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,19 +1,110 @@
 "use client";
 
-import { useState } from "react";
-import Select from "../Select";
-import { Filters } from "@/types";
+import { manufacturers } from "@/utils";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 
 export default function SearchBar() {
-  const [datasForm, setDatasForm] = useState<Filters>({
+  const [datasForm, setDatasForm] = useState<{
+    manufacturer: string;
+    model: string;
+  }>({
     manufacturer: "",
-    year: 0,
     model: "",
-    fuel: "",
   });
+  const router = useRouter();
+
+  function handleSubmitForm(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (datasForm.manufacturer.trim() === "" && datasForm.model.trim() === "") {
+      alert(
+        "Por favor entre com um fabricante ou modelo para pesquisar no catálogo."
+      );
+      return;
+    }
+    updateSearchParams(datasForm.model, datasForm.manufacturer);
+  }
+
+  function updateSearchParams(model: string, manufacturer: string) {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("modelo", model);
+    } else {
+      searchParams.delete("modelo");
+    }
+
+    if (manufacturer) {
+      searchParams.set("fabricante", manufacturer);
+    } else {
+      searchParams.delete("fabricante");
+    }
+
+    const newPathName = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathName);
+  }
+
   return (
-    <form>
-      <Select />
+    <form onSubmit={handleSubmitForm}>
+      <div>
+        <div>
+          <Image
+            src="/assets/car-logo.svg"
+            alt="Logo Car"
+            width={32}
+            height={32}
+          />
+          <select
+            title="Escolha uma fabricante"
+            aria-label="Escolha uma fabricante"
+            value={datasForm.manufacturer}
+            onChange={(e) => {
+              setDatasForm({ ...datasForm, manufacturer: e.target.value });
+            }}
+          >
+            <option value="">--Escolha Fabricante--</option>
+            {manufacturers.map((manufacturer, index) => (
+              <option value={manufacturer.toLowerCase()} key={index}>
+                {manufacturer}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <Image
+            src="/assets/model-icon.png"
+            alt="Icon Model Car"
+            width={32}
+            height={32}
+          />
+          <input
+            type="text"
+            aria-label="Entre com um modelo"
+            title="Entre com um modelo"
+            placeholder="Entre com um modelo"
+            value={datasForm.model}
+            onChange={(e) => {
+              setDatasForm({ ...datasForm, model: e.target.value });
+            }}
+          />
+        </div>
+      </div>
+      <button
+        type="submit"
+        title="Filtrar Catalogo"
+        aria-label="Filtrar Catalogo"
+      >
+        <Image
+          src="/assets/magnifying-glass.svg"
+          alt="Icon pesquisar"
+          width={32}
+          height={32}
+        />
+      </button>
     </form>
   );
 }

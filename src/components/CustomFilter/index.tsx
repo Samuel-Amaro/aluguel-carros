faab812b240f96cb8b515b107aa6755cf95e402f
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-28 11:20:54 -0300

    create componente CustomFilter

---
+++ src/components/CustomFilter/index.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { updateSearchParams } from "@/utils";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+
+export default function CustomFilter({
+  options,
+  title,
+  className,
+}: {
+  options: { label: string; value: string }[];
+  title: string;
+  className?: string;
+}) {
+  const router = useRouter();
+  const [optionSelected, setOptionSelected] = useState<string>(
+    options[0].value
+  );
+
+  const handleSearchParams = useCallback(
+    (title: string, value: string) => {
+      const newPathName = updateSearchParams(
+        title.toLowerCase(),
+        value.toLowerCase()
+      );
+      router.push(newPathName);
+    },
+    [router]
+  );
+
+  return (
+    <select
+      title={title}
+      aria-label={title}
+      value={optionSelected}
+      onChange={(e) => {
+        setOptionSelected(e.target.value);
+        handleSearchParams(title, e.target.value);
+      }}
+    >
+      <option value="">--{title}--</option>
+      {options.map((option, index) => (
+        <option key={index} value={option.value.toLowerCase()}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
+}

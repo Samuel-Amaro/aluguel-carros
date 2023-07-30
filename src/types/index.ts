c8faf308cc6cbd73418288f4a7fc7a27624f24df
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-30 16:22:28 -0300

    add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,22 @@
 export type Filters = {
-    manufacturer?: string;
-    year?: number;
-    model?: string;
-    fuel?: string;
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  fuel?: string;
+  limit?: number;
+};
+
+export type Cars = {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 };

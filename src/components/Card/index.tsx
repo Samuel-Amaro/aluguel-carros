02ba0708796f529fe045d15a9fb55c6a0d8b3170
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-31 10:07:04 -0300

    add icons datas card, e usando blur image e placeholder

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,10 @@
 import { Cars } from "@/types";
-import { calculateCarRent, generateCarImageUrl } from "@/utils";
+import {
+  calculateCarRent,
+  generateCarImageUrl,
+  shimer,
+  toBase64,
+} from "@/utils";
 import Image from "next/image";
 import styles from "./styles.module.css";
 
@@ -22,12 +27,35 @@ export default function Card({ datas }: { datas: Cars }) {
           alt="Imagem carro"
           width={400}
           height={160}
+          placeholder="blur"
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(
+            shimer(400, 160)
+          )}`}
         />
       </div>
       <div>
-        <p>{datas.transmission}</p>
-        <p>{datas.drive}</p>
-        <p>{datas.city_mpg}</p>
+        <div>
+          <Image
+            src="/assets/steering-wheel.svg"
+            width={20}
+            height={20}
+            alt="icone car"
+          />{" "}
+          <p>{datas.transmission === "a" ? "Automatic" : "Manual"}</p>
+        </div>
+        <div>
+          <Image src="/assets/tire.svg" width={20} height={20} alt="icon" />
+          <p>{datas.drive.toUpperCase()}</p>
+        </div>
+        <div>
+          <Image
+            src="/assets/gas.svg"
+            width={18}
+            height={20}
+            alt="icone de estação de gás"
+          />
+          <p>{datas.city_mpg} MPG</p>
+        </div>
       </div>
     </div>
   );

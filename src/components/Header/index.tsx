2072582d8961e6fae420a4b47b3e16827b4d3079
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-31 11:26:13 -0300

    add logo in Header componente

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
+
+Image;
 export default function Header({ title }: { title: string }) {
   return (
     <header>
+      <Image src="/assets/logo.svg" alt="logo" width={25} height={25} />
       <h1>{title}</h1>
     </header>
   );

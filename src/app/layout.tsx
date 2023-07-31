46a58b8d986802eb29e762c3d03b55a5faee52d1
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-31 11:27:38 -0300

    add Header, Footer componentes in layout root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,28 @@
+import Header from "@/components/Header";
 import "../styles/globals.css";
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Aluguel de Carros',
-  description: 'Website Aluguel de carros',
-}
+  title: "Aluguel de Carros",
+  description: "Website Aluguel de carros",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header title="Aluguel De Carros" />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }

7a17d59ecdafe12672781eea7b13a764f5c0387f
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-07-31 10:06:23 -0300

    add componente ShowMore, que criar pagination infinete

---
+++ src/components/ShowMore/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { updateSearchParams } from "@/utils";
+import { useRouter } from "next/navigation";
+
+export default function ShowMore({
+  pageNumber,
+  isNext,
+}: {
+  pageNumber: number;
+  isNext: boolean;
+}) {
+  const router = useRouter();
+
+  function handleNavigation() {
+    const nl = (pageNumber + 1) * 10;
+
+    const newPathname = updateSearchParams("limit", `${nl}`);
+
+    router.push(newPathname);
+  }
+
+  return (
+    !isNext && (
+      <button
+        type="button"
+        title="Mostrar mais"
+        aria-label="Mostrar mais"
+        onClick={handleNavigation}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" || e.key === "") {
+            handleNavigation();
+          }
+        }}
+      >
+        Mostrar Mais
+      </button>
+    )
+  );
+}

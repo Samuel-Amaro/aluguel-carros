d478ec6ca8dfb04bcda6f2a65662d5c797061cce
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-01 11:20:08 -0300

    usando componente Modal, e renderizando com portal, e add state para o modal, com button

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Cars } from "@/types";
 import {
   calculateCarRent,
@@ -7,59 +9,82 @@ import {
 } from "@/utils";
 import Image from "next/image";
 import styles from "./styles.module.css";
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import Modal from "../Modal";
 
 export default function Card({ datas }: { datas: Cars }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
-    <div>
-      <header>
-        <h1>
-          {datas.make} {datas.model}
-        </h1>
-      </header>
-      <p>
-        <span>R$</span>
-        <span>{calculateCarRent(datas.city_mpg, datas.year)}</span>
-        <span>dia</span>
-      </p>
-      <div className={styles.wrapperImage}>
-        <Image
-          src={generateCarImageUrl(datas)}
-          alt="Imagem carro"
-          width={400}
-          height={160}
-          placeholder="blur"
-          blurDataURL={`data:image/svg+xml;base64,${toBase64(
-            shimer(400, 160)
-          )}`}
-        />
-      </div>
+    <>
       <div>
-        <div>
+        <header>
+          <h1>
+            {datas.make} {datas.model}
+          </h1>
+        </header>
+        <p>
+          <span>R$</span>
+          <span>{calculateCarRent(datas.city_mpg, datas.year)}</span>
+          <span>dia</span>
+        </p>
+        <div className={styles.wrapperImage}>
           <Image
-            src="/assets/steering-wheel.svg"
-            width={20}
-            height={20}
-            alt="icone car"
-          />{" "}
-          <p>{datas.transmission === "a" ? "Automatic" : "Manual"}</p>
-        </div>
-        <div>
-          <Image src="/assets/tire.svg" width={20} height={20} alt="icon" />
-          <p>{datas.drive.toUpperCase()}</p>
-        </div>
-        <div>
-          <Image
-            src="/assets/gas.svg"
-            width={18}
-            height={20}
-            alt="icone de estação de gás"
+            src={generateCarImageUrl(datas)}
+            alt="Imagem carro"
+            width={400}
+            height={160}
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(
+              shimer(400, 160)
+            )}`}
           />
-          <p>{datas.city_mpg} MPG</p>
         </div>
+        <div>
+          <div>
+            <Image
+              src="/assets/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="icone car"
+            />{" "}
+            <p>{datas.transmission === "a" ? "Automatic" : "Manual"}</p>
+          </div>
+          <div>
+            <Image src="/assets/tire.svg" width={20} height={20} alt="icon" />
+            <p>{datas.drive.toUpperCase()}</p>
+          </div>
+          <div>
+            <Image
+              src="/assets/gas.svg"
+              width={18}
+              height={20}
+              alt="icone de estação de gás"
+            />
+            <p>{datas.city_mpg} MPG</p>
+          </div>
+        </div>
+        <button
+          type="button"
+          title="Visualizar"
+          aria-label="Visualizar"
+          onClick={() => setModalIsOpen(true)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === "") setModalIsOpen(true);
+          }}
+        >
+          Visualizar
+        </button>
       </div>
-      <button type="button" title="Visualizar" aria-label="Visualizar">
-        Visualizar
-      </button>
-    </div>
+      {modalIsOpen &&
+        createPortal(
+          <Modal
+            datas={datas}
+            isOpen={modalIsOpen}
+            onClose={() => setModalIsOpen(false)}
+          />,
+          document.body
+        )}
+    </>
   );
 }

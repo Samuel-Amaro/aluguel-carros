4fc5e6d20cca25b291afc50dabe402ba4fe7f0bf
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-02 11:43:18 -0300

    add font e weights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
-import Header from "@/components/Header";
+import "../styles/normalize.css";
 import "../styles/globals.css";
+import Header from "@/components/Header";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Aluguel de Carros",

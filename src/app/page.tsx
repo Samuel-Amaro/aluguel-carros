2ad132c34309e34f5956ccdb250f1fd7be0e597f
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-03 11:20:10 -0300

    add styles page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,42 +27,51 @@ export default async function Home({
   });
 
   const isDataEmpty = !Array.isArray(datas) || datas.length < 1 || !datas;
+  
 
   return (
-    <main>
-      <div>
-        <Ads
-          title="A Melhor Plataforma para Aluguel de Carros"
-          description="Facilidade em fazer um aluguel de carro com segurança e
-            confiabilidade. Claro a um preço baixo."
-          tag="Carro Alugado"
-        >
+    <main className={styles.main}>
+      <div className={styles.mainContainerAds}>
+        <div className={styles.ads1}>
+          <h2 className={styles.ads1Title}>
+            A Melhor Plataforma para Aluguel de Carros
+          </h2>
+          <p className={styles.ads1Description}>
+            Facilidade em fazer um aluguel de carro com segurança e
+            confiabilidade. Claro a um preço baixo.
+          </p>
+          <span className={styles.ads1Tag}>Carro Alugado</span>
           <Image
             src="/assets/ads1.png"
             alt="Imagen Ads carro alugado"
             width={406}
             height={116}
+            className={styles.adsImage}
           />
-        </Ads>
-        <Ads
-          title="Maneira fácil de alugar um carro a um preço baixo"
-          description="Fornecendo serviços de aluguel de carros baratos e instalações
-            seguras e confortáveis."
-          tag="Carro Alugado"
-        >
+        </div>
+        <div className={styles.ads2}>
+          <h2 className={styles.ads2Title}>
+            Maneira fácil de alugar um carro a um preço baixo
+          </h2>
+          <p className={styles.ads2Description}>
+            Fornecendo serviços de aluguel de carros baratos e instalações
+            seguras e confortáveis.
+          </p>
+          <span className={styles.ads2Tag}>Carro Alugado</span>
           <Image
             src="/assets/ads2.png"
             alt="Imagem Ads carro alugado"
             width={340}
             height={108}
+            className={styles.adsImage}
           />
-        </Ads>
+        </div>
       </div>
-      <h2>Catálogo de carros</h2>
-      <p>Explore os carros que você pode gostar</p>
-      <div>
+      <h2 className={styles.mainTitle}>Catálogo de carros</h2>
+      <p className={styles.mainDescription}>Explore os carros que você pode gostar</p>
+      <div className={styles.mainSearch}>
         <SearchBar />
-        <div>
+        <div className={styles.mainContainerCustomFilters}>
           <CustomFilter options={yearsOfProduction} title="Year" />
           <CustomFilter options={fuels} title="Fuel" />
         </div>
@@ -72,9 +81,9 @@ export default async function Home({
           <Suspense
             fallback={<SkeletonCatalogueCar limit={searchParams.limit || 10} />}
           >
-            <ul>
+            <ul className={styles.mainList}>
               {datas?.map((car, index) => (
-                <li key={index}>{<Card datas={car} />}</li>
+                <li key={index} className={styles.mainListItem}>{<Card datas={car} />}</li>
               ))}
             </ul>
           </Suspense>

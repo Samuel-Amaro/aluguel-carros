002107a48594995f9f9a3bdbcfd259d5b4cc0efc
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-03 11:17:21 -0300

    add styles componente Card, arrumando componente image, e refatornado estrutura html de dados

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -12,56 +12,72 @@ import styles from "./styles.module.css";
 import { useState } from "react";
 import { createPortal } from "react-dom";
 import Modal from "../Modal";
+import { MULTIPLYING_FACTOR_MILES } from "@/utils";
 
 export default function Card({ datas }: { datas: Cars }) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <>
-      <div>
+      <div className={styles.card}>
         <header>
-          <h1>
+          <h1 className={styles.cardTitle}>
             {datas.make} {datas.model}
           </h1>
         </header>
-        <p>
-          <span>R$</span>
-          <span>{calculateCarRent(datas.city_mpg, datas.year)}</span>
-          <span>dia</span>
+        <p className={styles.cardPrice}>
+          <span className={styles.cardPriceValue}>
+            R${calculateCarRent(datas.city_mpg, datas.year)}/{" "}
+          </span>
+          <span className={styles.cardPriceTime}>dia</span>
         </p>
         <div className={styles.wrapperImage}>
           <Image
             src={generateCarImageUrl(datas)}
             alt="Imagem carro"
-            width={400}
-            height={160}
+            /*width={400}
+            height={160}*/
             placeholder="blur"
             blurDataURL={`data:image/svg+xml;base64,${toBase64(
               shimer(400, 160)
             )}`}
+            fill
+            className={styles.cardImage}
           />
         </div>
-        <div>
-          <div>
+        <div className={styles.cardContainerDatas}>
+          <div className={styles.cardData}>
             <Image
               src="/assets/steering-wheel.svg"
               width={20}
               height={20}
               alt="icone car"
+              className={styles.cardDataIcon}
             />{" "}
-            <p>{datas.transmission === "a" ? "Automatic" : "Manual"}</p>
+            <p className={styles.cardDataValue}>
+              {datas.transmission === "a" ? "Automático" : "Manual"}
+            </p>
           </div>
-          <div>
-            <Image src="/assets/tire.svg" width={20} height={20} alt="icon" />
-            <p>{datas.drive.toUpperCase()}</p>
+          <div className={styles.cardData}>
+            <Image
+              src="/assets/tire.svg"
+              width={20}
+              height={20}
+              alt="icon"
+              className={styles.cardDataIcon}
+            />
+            <p className={styles.cardDataValue}>{datas.drive.toUpperCase()}</p>
           </div>
-          <div>
+          <div className={styles.cardData}>
             <Image
               src="/assets/gas.svg"
               width={18}
               height={20}
               alt="icone de estação de gás"
+              className={styles.cardDataIcon}
             />
-            <p>{datas.city_mpg} MPG</p>
+            <p className={styles.cardDataValue}>
+              {(MULTIPLYING_FACTOR_MILES * datas.city_mpg).toFixed(2)} Km/L
+            </p>
           </div>
         </div>
         <button
@@ -72,6 +88,7 @@ export default function Card({ datas }: { datas: Cars }) {
           onKeyDown={(e) => {
             if (e.key === "Enter" || e.key === "") setModalIsOpen(true);
           }}
+          className={styles.cardButton}
         >
           Visualizar
         </button>

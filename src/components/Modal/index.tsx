063aa6bca8aed52e1737f7e06c7c1d98517c42fe
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-04 11:52:51 -0300

    alterando estrutura html e add styles

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,7 +5,7 @@ import { generateCarImageUrl } from "@/utils";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { MULTIPLYING_FACTOR_MILES } from "@/utils";
-
+import styles from "./styles.module.css";
 
 function getDrive(type: string) {
   if (type === "fwd") return "Tração dianteira";
@@ -28,38 +28,46 @@ export default function Modal({
     <Image
       src={generateCarImageUrl(datas)}
       alt="imagem do carro normal"
-      /*fill*/ priority
+      /*fill*/
+      priority
       width={300}
       height={100}
       key={`image-0`}
       title="Visualizar Image do carro com angulo normal"
+      className={styles.modalImage}
     />,
     <Image
       src={generateCarImageUrl(datas, "29")}
       alt="imagem do carro com angulo de 29 graus"
-      /*fill*/ priority
+      /*fill*/
+      priority
       width={300}
       height={100}
       key={`image-1`}
       title="Visualizar Imagen do carro com angulo de 29 graus"
+      className={styles.modalImage}
     />,
     <Image
       src={generateCarImageUrl(datas, "33")}
       alt="imagem do carro com angulo de 33 graus"
-      /*fill*/ priority
+      /*fill*/
+      priority
       width={300}
       height={100}
       key={`image-2`}
       title="Visualizar Imagen do carro com angulo de 33 graus"
+      className={styles.modalImage}
     />,
     <Image
       src={generateCarImageUrl(datas, "13")}
       alt="imagem do carro com angulo de 13 graus"
-      /*fill*/ priority
+      /*fill*/
+      priority
       width={300}
       height={100}
       key={`image-3`}
       title="Visualizar Imagen do carro com angulo de 13 graus"
+      className={styles.modalImage}
     />,
   ];
 
@@ -85,10 +93,14 @@ export default function Modal({
         document.body.classList.remove("has-dialog");
       }
     }
+
+    return () => {
+      document.body.classList.remove("has-dialog");
+    };
   }, [isOpen]);
 
   return (
-    <dialog ref={refDialog} onClose={handleClose}>
+    <dialog ref={refDialog} onClose={handleClose} className={styles.modal}>
       <button
         onClick={() => handleClose()}
         onKeyDown={(e) => {
@@ -102,11 +114,15 @@ export default function Modal({
           height={24}
         />
       </button>
-      <div>
-        <div aria-live="polite" aria-atomic="true">
+      <div className={styles.modalLightbox}>
+        <div
+          aria-live="polite"
+          aria-atomic="true"
+          className={styles.modalAreaViewImage}
+        >
           {imageSelectedToView}
         </div>
-        <div>
+        <div className={styles.modalContainerImages}>
           {imagesLightbox.map((image, index) => (
             <div
               key={index}
@@ -116,77 +132,164 @@ export default function Modal({
                   setImageSelectedToView(imagesLightbox[index]);
                 }
               }}
+              className={styles.modalWrapperImage}
             >
               {image}
             </div>
           ))}
         </div>
       </div>
-      <h3>{`${datas.make} ${datas.model}`}</h3>
+      <h3 className={styles.modalTitle}>{`${datas.make} ${datas.model}`}</h3>
       <div role="table" aria-label="Dados do Carro Selecionado">
-        <div role="rowgroup">
-          <div role="row">
-            <p role="columnheader" aria-sort="none">
-              Km/L Cidade
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Classe
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Combinação Km/L
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Cilindros
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Deslocamento
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Transmissão de acionamento
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Tipo de combustível
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Km/L Estrada/Rodovia
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Fabricante
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Modelo
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Transmissão
-            </p>
-            <p role="columnheader" aria-sort="none">
-              Ano
-            </p>
-          </div>
-          <div role="row">
-            <p role="cell">
-              {(MULTIPLYING_FACTOR_MILES * datas.city_mpg).toFixed(2)}{" "}
-              (aproximadamente)
-            </p>
-            <p role="cell">{datas.class}</p>
-            <p role="cell">
-              {(MULTIPLYING_FACTOR_MILES * datas.combination_mpg).toFixed(2)}{" "}
-              (aproximadamente)
-            </p>
-            <p role="cell">{datas.cylinders}</p>
-            <p role="cell">{datas.displacement}</p>
-            <p role="cell">{getDrive(datas.drive)}</p>
-            <p role="cell">{datas.fuel_type}</p>
-            <p role="cell">
-              {(MULTIPLYING_FACTOR_MILES * datas.highway_mpg).toFixed(2)}{" "}
-              (aproximadamente)
-            </p>
-            <p role="cell">{datas.make}</p>
-            <p role="cell">{datas.model}</p>
-            <p role="cell">
-              {datas.transmission === "a" ? "Automático" : "Manual"}
-            </p>
-            <p role="cell">{datas.year}</p>
+        <div role="rowgroup" className={styles.modalTableRowGroup}>
+          <div role="row" className={styles.modalTableRow}>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Km/L Cidade
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {(MULTIPLYING_FACTOR_MILES * datas.city_mpg).toFixed(2)}{" "}
+                (aproximadamente)
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Classe
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {datas.class}
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Combinação Km/L
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {(MULTIPLYING_FACTOR_MILES * datas.combination_mpg).toFixed(2)}{" "}
+                (aproximadamente)
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Cilindros
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {datas.cylinders}
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Deslocamento
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {datas.displacement}
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Transmissão de acionamento
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {getDrive(datas.drive)}
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Tipo de combustível
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {datas.fuel_type}
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Km/L Estrada/Rodovia
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {(MULTIPLYING_FACTOR_MILES * datas.highway_mpg).toFixed(2)}{" "}
+                (aproximadamente)
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Fabricante
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {datas.make}
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Modelo
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {datas.model}
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Transmissão
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {datas.transmission === "a" ? "Automático" : "Manual"}
+              </p>
+            </div>
+            <div className={styles.modalTableContainerRow}>
+              <p
+                role="columnheader"
+                aria-sort="none"
+                className={styles.modalTableHeader}
+              >
+                Ano
+              </p>
+              <p role="cell" className={styles.modalTableCell}>
+                {datas.year}
+              </p>
+            </div>
           </div>
         </div>
       </div>

e421deecbaade82b0d47b75646aaa695e5495951
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-08-09 15:30:46 -0300

    add router handler para fetch datas

---
+++ src/app/api/route.ts
@@ -0,0 +1,21 @@
+import { fetcher } from "@/api/cars";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  console.log(request.url);
+  const searchParamsValues = {
+    manufacturer: searchParams.get("manufacturer")
+      ? (searchParams.get("manufacturer") as string)
+      : "",
+    year: searchParams.get("year") ? Number(searchParams.get("year")) : 2022,
+    model: searchParams.get("model")
+      ? (searchParams.get("model") as string)
+      : "",
+    fuel: searchParams.get("fuel") ? (searchParams.get("fuel") as string) : "",
+    limit: searchParams.get("limit") ? Number(searchParams.get("limit")) : 10,
+  };
+
+  const datas = await fetcher(searchParamsValues);
+  return NextResponse.json(datas);
+}
